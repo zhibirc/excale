f48229c91a003cff7e7bf98b160a2e83ba7a2d30
Yaroslav <y.surilov@infomir.com>
2020-02-28 14:00:14 +0200

    add basic module implementation, update ESLint configuration, readme

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,22 @@
+'use strict';
+
+
 module.exports = {
-    extends: require.resolve('cjs-eslint')
+    extends: require.resolve('cjs-eslint'),
+
+    env: {
+        es6: true,
+        browser: true
+    },
+
+    rules: {
+        'one-var': 0,
+        'padding-line-between-statements': [
+            'error',
+            {blankLine: 'always', prev: '*', next: ['directive', 'return', 'export', 'cjs-export', 'try', 'function']},
+            {blankLine: 'always', prev: ['directive', 'export', 'cjs-export', 'try', 'function'], next: '*'},
+            {blankLine: 'any',    prev: 'directive', next: 'directive'}
+        ],
+        quotes: ['error', 'single', {allowTemplateLiterals: true, avoidEscape: true}]
+    }
 };
---
+++ src/main.js
@@ -0,0 +1,20 @@
+/**
+ * Calendar/datepicker implementation.
+ *
+ * Component that allows users to view and select dates.
+ *
+ * @module
+ *
+ * @author Yaroslav Surilov <>
+ */
+
+'use strict';
+
+class Excale {
+    constructor ( config = {} ) {
+        const bindElement = config.bindTo || document.body;
+    }
+}
+
+
+module.exports = Excale;
