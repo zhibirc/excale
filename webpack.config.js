b8cf748f93c7de13aaae06975bc3995ea7e9e403
Yaroslav <y.surilov@infomir.com>
2020-03-02 16:54:44 +0200

    update Webpack config and build

--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,28 @@
 
 const path = require('path');
 
+const TerserWebpackPlugin = require('terser-webpack-plugin');
+
 
 module.exports = {
-    entry: '/src/main.js',
+    entry: './src/main.js',
     output: {
         path: path.resolve(__dirname, 'build'),
         filename: 'index.min.js'
+    },
+    optimization: {
+        minimize: true,
+        minimizer: [
+            new TerserWebpackPlugin({
+                terserOptions: {
+                    output: {
+                        comments: false
+                    }
+                },
+                extractComments: false,
+                parallel: true
+            })
+        ]
     },
     mode: 'production'
 };
