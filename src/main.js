f42637656f14ae32c82a6be76895a623aca68505
Yaroslav <y.surilov@infomir.com>
2020-03-02 14:39:25 +0200

    update public API, add locale, basic date preparing

--- src/main.js
+++ src/main.js
@@ -10,9 +10,31 @@
 
 'use strict';
 
+const locale = {
+    monthsShort: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+    months:      ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
+    daysShort:   ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"],
+    days:        ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Monday"]
+};
+
+function prepareDate ( date ) {
+    return {
+        day:  date.getDay(),
+        days: 32 - (new Date(date.setDate(32))).getDate(),
+    };
+}
+
 class Excale {
     constructor ( config = {} ) {
+        const date = config.year && config.month ? new Date(config.year, config.month - 1) : new Date();
+        const {day, days} = prepareDate(date);
+
         const bindElement = config.bindTo || document.body;
+    }
+
+    pick ( config = {} ) {
+        const date = config.year && config.month ? new Date(config.year, config.month - 1) : new Date();
+        const {day, days} = prepareDate(date);
     }
 }
 
