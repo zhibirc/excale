2556924a3a1afa93d1207761f5ec3b393e39de28
Yaroslav <y.surilov@infomir.com>
2020-03-03 15:07:20 +0200

    add element generation

--- src/main.js
+++ src/main.js
@@ -10,6 +10,8 @@
 
 'use strict';
 
+const BASE_CLASS_PREFIX = 'excale';
+
 const locale = {
     monthsShort: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
     months:      ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
@@ -17,74 +19,59 @@ const locale = {
     days:        ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Monday']
 };
 
-const div = (content, classList = []) => {
+const div = (content, className) => {
     const $div = document.createElement('div');
 
-    $div.classList.add(...classList);
+    className && $div.classList.add(className);
     $div.textContent = content;
 
     return $div;
 };
 
-const a = (content, classList = []) => {
+const a = (content, className) => {
     const $a = document.createElement('a');
 
     $a.setAttribute('href', '#');
-    $a.classList.add(...classList);
+    className && $a.classList.add(className);
     $a.textContent = content;
 
     return $a;
 };
 
 function buildMonthDOM ( config ) {
-    const $container    = div();
-    const $header       = div(config.monthName);
-    const $weekdayNames = div();
-    const $days         = div();
+    const $container    = div(null, `${BASE_CLASS_PREFIX}-month-item`);
+    const $header       = div(config.monthName, `${BASE_CLASS_PREFIX}-month-item__header`);
+    const $weekdayNames = div(null, `${BASE_CLASS_PREFIX}-month-item__weekday-names`);
+    const $days         = div(null, `${BASE_CLASS_PREFIX}-month-item__days`);
+
+    config.dayNames.forEach(dayName => $weekdayNames.appendChild(div(dayName)));
+    Array.from({length: config.dayStart}, () => $days.appendChild(div()));
+    Array.from({length: config.daysCount}, (item, index) => $days.appendChild(a(index + 1, `${BASE_CLASS_PREFIX}-day-item`)));
+    Array.from({length: ++config.daysCount % 7}, () => () => $days.appendChild(div()));
 
     $container.appendChild($header);
     $container.appendChild($weekdayNames);
     $container.appendChild($days);
 
-    config.dayNames.forEach(dayName => $weekdayNames.appendChild(div(dayName)));
-
-    for ( let index = 0; index < config.days; index += 1 ) {
-        // todo: implement
-    }
-
     return $container;
-}
-
-function prepareDate ( date ) {
-    return {
-        day:  date.getDay(),
-        days: 32 - (new Date(date.setDate(32))).getDate()
-    };
 }
 
 class Excale {
     constructor ( config = {} ) {
         const date = config.year && config.month ? new Date(config.year, config.month - 1) : new Date();
-        const {day, days} = prepareDate(date);
-        const bindElement = config.bindTo || document.body;
 
         this.dom = {
             $month: buildMonthDOM({
-                monthName: locale.monthsShort[date.getMonth()],
+                monthName: locale.months[date.getMonth()],
                 dayNames:  locale.daysShort,
-                days,
-                day
+                dayStart:  date.getDay(),
+                daysCount: 32 - (new Date(date.setDate(32))).getDate()
             })
         };
 
-        bindElement.appendChild(this.dom.$month);
-    }
-
-    pick ( config = {} ) {
-        const date = config.year && config.month ? new Date(config.year, config.month - 1) : new Date();
-        const {day, days} = prepareDate(date);
+        (config.bindTo || document.body).appendChild(this.dom.$month);
     }
 }
 
 
-module.exports = Excale;
+//module.exports = Excale;
